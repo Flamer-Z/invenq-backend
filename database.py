c1a993f13bd15b5bae56846f6399a7b00e803db4
nireg <59962930+Flamer-Z@users.noreply.github.com>
2026-05-03 09:00:28 +0800

    Update database.py

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not found ❌")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 
